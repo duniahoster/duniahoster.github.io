e00c4444222682f47d794993f2142f4cea4fb28d
duniahoster <duniahoster@gmail.com>
2021-04-26 01:39:33 +0700

    ubah route create ke store

--- core/resources/views/admin/users/create.blade.php
+++ core/resources/views/admin/users/create.blade.php
@@ -9,7 +9,7 @@
             <div class="card-body">
                 <h5 class="card-title mb-50 border-bottom pb-2">Add New User</h5>
 
-                <form action="create" method="POST">
+                <form action="store" method="POST">
                     @csrf
 
                     <div class="row">
