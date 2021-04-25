4cf3cf20f12f17f06b6a582b0dbe3967960fe6a4
duniahoster <duniahoster@gmail.com>
2021-04-25 22:20:17 +0700

    edit form create user

--- core/resources/views/admin/users/create.blade.php
+++ core/resources/views/admin/users/create.blade.php
@@ -2,143 +2,112 @@
 
 @section('panel')
 <div class="row mb-none-30">
-    <div class="card mt-50">
-        <div class="card-body">
-            <h5 class="card-title mb-50 border-bottom pb-2">@lang('Add New User')</h5>
 
-            <form action="/users" method="POST">
-                @csrf
+    <div class="col-xl-9 col-lg-7 col-md-7 mb-30">
 
-                <div class="row">
-                    <div class="col-md-3">
-                        <div class="form-group ">
-                            <label for="username" class="form-control-label font-weight-bold">@lang('Username') <span class="text-danger">*</span></label>
-                            <input class="form-control" type="text @error('username') is-invalid @enderror" id="username" placeholder="Username" name="username">
-                            @error('username')<div class="invalid-feedback">{{ $message }}</div>@enderror
+        <div class="card mt-50">
+            <div class="card-body">
+                <h5 class="card-title mb-50 border-bottom pb-2">Add New User</h5>
+
+                <form action="admin.users.create" method="POST">
+                    @csrf
+
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group ">
+                                <label for="username" class="form-control-label font-weight-bold">Username<span class="text-danger">*</span></label>
+                                <input class="form-control" type="text" id="username" name="username" placeholder="Username">
+                            </div>
                         </div>
-                    </div>
 
-                    <div class="col-md-4">
-                        <div class="form-group ">
-                            <label for="firstname" class="form-control-label font-weight-bold">@lang('First Name') <span class="text-danger">*</span></label>
-                            <input class="form-control" type="text @error('firstname') is-invalid @enderror" id="firstname" placeholder="First Name" name="firstname">
-                            @error('firstname')<div class="invalid-feedback">{{ $message }}</div>@enderror
+                        <div class="col-md-6">
+                            <div class="form-group ">
+                                <label for="firstname" class="form-control-label font-weight-bold">First Name<span class="text-danger">*</span></label>
+                                <input class="form-control" type="text" id="firstname" name="firstname" placeholder="First Name">
+                            </div>
                         </div>
-                    </div>
 
-                    <div class="col-md-5">
-                        <div class="form-group ">
-                            <label for="lastname" class="form-control-label font-weight-bold">@lang('Last Name') <span class="text-danger">*</span></label>
-                            <input class="form-control" type="text @error('lastname') is-invalid @enderror" id="lastname" placeholder="Last Name" name="lastname">
-                            @error('lastname')<div class="invalid-feedback">{{ $message }}</div>@enderror
+                        <div class="col-md-6">
+                            <div class="form-group ">
+                                <label for="lastname" class="form-control-label font-weight-bold">Last Name<span class="text-danger">*</span></label>
+                                <input class="form-control" type="text" id="lastname" name="lastname" placeholder="Last Name">
+                            </div>
                         </div>
-                    </div>
-                </div>
 
-                <div class="row">
-                    <div class="col-md-6">
-                        <div class="form-group ">
-                            <label for="email" class="form-control-label font-weight-bold">@lang('Email Addres') <span class="text-danger">*</span></label>
-                            <input class="form-control" type="text @error('email') is-invalid @enderror" id="email" placeholder="Email Addres" name="email">
-                            @error('email')<div class="invalid-feedback">{{ $message }}</div>@enderror
+                        <div class="col-md-6">
+                            <div class="form-group ">
+                                <label for="email" class="form-control-label font-weight-bold">Email Addres<span class="text-danger">*</span></label>
+                                <input class="form-control" type="text" id="email" name="email" placeholder="Email Addres">
+                            </div>
                         </div>
-                    </div>
 
-                    <div class="col-md-6">
-                        <div class="form-group ">
-                            <label for="mobile" class="form-control-label font-weight-bold">@lang('Mobile Number') <span class="text-danger">*</span></label>
-                            <input class="form-control" type="text @error('mobile') is-invalid @enderror" id="mobile" placeholder="Mobile Number" name="mobile">
-                            @error('mobile')<div class="invalid-feedback">{{ $message }}</div>@enderror
+                        <div class="col-md-6">
+                            <div class="form-group ">
+                                <label for="mobile" class="form-control-label font-weight-bold">Mobile Number<span class="text-danger">*</span></label>
+                                <input class="form-control" type="text" id="mobile" name="mobile" placeholder="Mobile Number">
+                            </div>
                         </div>
-                    </div>
-                </div>
 
-                <div class="row mt-4">
-                    <div class="col-md-12">
-                        <div class="form-group ">
-                            <label for="address" class="form-control-label font-weight-bold">@lang('Address') </label>
-                            <input class="form-control" type="text @error('address') is-invalid @enderror" id="address" placeholder="Enter Address" name="address">
-                            <small class="form-text text-muted"><i class="las la-info-circle"></i> @lang('House number, street address')
-                            </small>
-                            @error('address')<div class="invalid-feedback">{{ $message }}</div>@enderror
+                        <div class="col-md-6">
+                            <div class="form-group ">
+                                <label for="akunmt5" class="form-control-label font-weight-bold">Nomor Akun MT5<span class="text-danger">*</span></label>
+                                <input class="form-control" type="text" id="akunmt5" name="akunmt5" placeholder="Nomor Akun Mt5">
+                            </div>
                         </div>
-                    </div>
 
-                    <div class="col-xl-3 col-md-6">
-                        <div class="form-group">
-                            <label for="city" class="form-control-label font-weight-bold">@lang('City') </label>
-                            <input class="form-control" type="text @error('city') is-invalid @enderror" id="city" placeholder="City" name="city">
-                            @error('city')<div class="invalid-feedback">{{ $message }}</div>@enderror
-                        </div>
-                    </div>
+                        <div class="row mt-4">
+                            <div class="col-md-12">
+                                <div class="col-md-6">
+                                    <div class="form-group ">
+                                        <label for="address" class="form-control-label font-weight-bold">Address<span class="text-danger">*</span></label>
+                                        <input class="form-control" type="text" id="address" name="address" placeholder="Nama jalan, Desa, RT/RW">
+                                    </div>
+                                </div>
 
-                    <div class="col-xl-3 col-md-6">
-                        <div class="form-group">
-                            <label for="state" class="form-control-label font-weight-bold">@lang('State') </label>
-                            <input class="form-control" type="text @error('state') is-invalid @enderror" id="state" placeholder="State" name="state">
-                            @error('state')<div class="invalid-feedback">{{ $message }}</div>@enderror
-                        </div>
-                    </div>
+                                <div class="col-xl-3 col-md-6">
+                                    <div class="form-group ">
+                                        <label for="city" class="form-control-label font-weight-bold">City<span class="text-danger">*</span></label>
+                                        <input class="form-control" type="text" id="city" name="city" placeholder="City">
+                                    </div>
+                                </div>
 
-                    <div class="col-xl-3 col-md-6">
-                        <div class="form-group">
-                            <label for="zip" class="form-control-label font-weight-bold">@lang('Zip/Postal') </label>
-                            <input class="form-control" type="text @error('zip') is-invalid @enderror" id="zip" placeholder="Zip/Postal" name="zip">
-                            @error('zip')<div class="invalid-feedback">{{ $message }}</div>@enderror
-                        </div>
-                    </div>
+                                <div class="col-xl-3 col-md-6">
+                                    <div class="form-group ">
+                                        <label for="state" class="form-control-label font-weight-bold">State<span class="text-danger">*</span></label>
+                                        <input class="form-control" type="text" id="state" name="state" placeholder="State">
+                                    </div>
+                                </div>
 
-                    <div class="col-xl-3 col-md-6">
-                        <div class="form-group">
-                            <label for="country" class="form-control-label font-weight-bold">@lang('Country') </label>
-                            <select name="country" class="form-control"> @include('partials.country') </select>
-                        </div>
-                    </div>
-                </div>
+                                <div class="col-xl-3 col-md-6">
+                                    <div class="form-group ">
+                                        <label for="zip" class="form-control-label font-weight-bold">Zip/Postal<span class="text-danger">*</span></label>
+                                        <input class="form-control" type="text" id="zip" name="zip" placeholder="Zip/Postal">
+                                    </div>
+                                </div>
 
-
-                <div class="row">
-                    <div class="form-group col-xl-4 col-md-6  col-sm-3 col-12">
-                        <label class="form-control-label font-weight-bold">@lang('Status') </label>
-                        <input type="checkbox" data-width="100%" data-onstyle="-success" data-offstyle="-danger" data-toggle="toggle" data-on="Active" data-off="Banned" data-width="100%" name="status" @if($user->status) checked @endif>
-                    </div>
-
-                    <div class="form-group  col-xl-4 col-md-6  col-sm-3 col-12">
-                        <label class="form-control-label font-weight-bold">@lang('Email Verification') </label>
-                        <input type="checkbox" data-width="100%" data-onstyle="-success" data-offstyle="-danger" data-toggle="toggle" data-on="Verified" data-off="Unverified" name="ev" @if($user->ev) checked @endif>
-
-                    </div>
-
-                    <div class="form-group  col-xl-4 col-md-6  col-sm-3 col-12">
-                        <label class="form-control-label font-weight-bold">@lang('SMS Verification') </label>
-                        <input type="checkbox" data-width="100%" data-onstyle="-success" data-offstyle="-danger" data-toggle="toggle" data-on="Verified" data-off="Unverified" name="sv" @if($user->sv) checked @endif>
-
-                    </div>
-                    <div class="form-group  col-md-6  col-sm-3 col-12">
-                        <label class="form-control-label font-weight-bold">@lang('2FA Status') </label>
-                        <input type="checkbox" data-width="100%" data-onstyle="-success" data-offstyle="-danger" data-toggle="toggle" data-on="Verified" data-off="Unverified" name="ts" @if($user->ts) checked @endif>
-                    </div>
-
-                    <div class="form-group  col-md-6  col-sm-3 col-12">
-                        <label class="form-control-label font-weight-bold">@lang('2FA Verification') </label>
-                        <input type="checkbox" data-width="100%" data-onstyle="-success" data-offstyle="-danger" data-toggle="toggle" data-on="Verified" data-off="Unverified" name="tv" @if($user->tv) checked @endif>
-                    </div>
-                </div>
+                                <div class="col-xl-3 col-md-6">
+                                    <div class="form-group ">
+                                        <label class="form-control-label font-weight-bold">@lang('Country') </label>
+                                        <select name="country" class="form-control"> @include('partials.country') </select>
+                                    </div>
+                                </div>
 
 
-                <div class="row mt-4">
-                    <div class="col-md-12">
-                        <div class="form-group">
-                            <button type="submit" class="btn btn--primary btn-block btn-lg">@lang('Add New User')
-                            </button>
-                        </div>
-                    </div>
+                                <div class="row mt-4">
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <button type="submit" class="btn btn--primary btn-block btn-lg">@lang('Save Changes')
+                                            </button>
+                                        </div>
+                                    </div>
 
-                </div>
-            </form>
+                                </div>
+                </form>
+            </div>
         </div>
     </div>
 </div>
+
 
 @endsection
 
