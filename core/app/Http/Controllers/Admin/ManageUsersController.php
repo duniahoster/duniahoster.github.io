2ee4c139db7541346d20486339a287a8a8a6efdf
duniahoster <duniahoster@gmail.com>
2021-04-26 04:07:30 +0700

    balikin ke user

--- core/app/Http/Controllers/Admin/ManageUsersController.php
+++ core/app/Http/Controllers/Admin/ManageUsersController.php
@@ -29,7 +29,19 @@ class ManageUsersController extends Controller
 
     public function storeUsers(Request $request)
     {
-        <h1>Coba tampilan</h1>
+        $request->validate([
+            'username' => 'required|unique',
+            'firstname' => 'required',
+            'lastname' => 'required',
+            'email' => 'required|email|unique',
+            'mobile' => 'required|numeric|unique',
+            'address' => 'required',
+            'city' => 'required',
+            'state' => 'required',
+            'zip/postal' => 'required'
+        ]);
+        User::create($request->all());
+        return redirect('admin.users')->with('status', 'User Added Successfully!');
     }
 
     public function allUsers()
