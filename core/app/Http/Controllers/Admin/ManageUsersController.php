cfee44f5aa46d627495da62fd2cb7e78f2865402
duniahoster <duniahoster@gmail.com>
2021-04-28 03:21:37 +0700

    memindahkan file create user dari folder users ke folder admin

--- core/app/Http/Controllers/Admin/ManageUsersController.php
+++ core/app/Http/Controllers/Admin/ManageUsersController.php
@@ -41,7 +41,7 @@ class ManageUsersController extends Controller
             'zip/postal' => 'required'
         ]);
         User::create($request->all());
-        return redirect('admin')->with('status', 'User Added Successfully!');
+        return redirect('admin.users')->with('status', 'User Added Successfully!');
         // return redirect('admin.users')->with('status', 'User Added Successfully!');
     }
 
